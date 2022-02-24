2da56af1db3e3b34e2fc850fe42b0f388b6aa959
Choe Hyonsu Gabrielle <choe.hyonsu.gabrielle@gmail.com>
2022-02-25 01:28:04 +0900

    add attention_mask

--- gabrielle/pretrained/model.py
+++ gabrielle/pretrained/model.py
@@ -11,40 +11,80 @@ class MLMConfig:
     EMBEDDING_DIM = 768
     FACTORIZED_DIM = 128
     CROSS_LAYER_SHARING = True
-    NUM_HEADS = 4
+    NUM_HEADS = 12
     FEEDFORWARD_DIM = EMBEDDING_DIM
-    NUM_LAYERS = 4
+    NUM_LAYERS = 12
     DROPOUT_RATE = 0.1
     BATCH_SIZE = 256
     LEARNING_RATE = 0.0001
 
 
-class FactorizedTokSegPositEmbeddingLayer(layers.Layer):
+class FactorizedEmbeddingLayer(layers.Layer):
     def __init__(self, max_length, vocab_size, embedding_dim, factorized_dim=64):
-        super(FactorizedTokSegPositEmbeddingLayer, self).__init__(name=self.__class__.__name__)
+        """
+        :param max_length: tokenizer.max_length
+        :param vocab_size: tokenizer.vocab_size for input_dim of Embedding
+        :param embedding_dim: final embedding output dim
+        :param factorized_dim: intermediate dim for factorized embedding parameterization
+        note that attention_mask will be computed from embedding.compute_mask(token_ids).
+        """
+        super(FactorizedEmbeddingLayer, self).__init__(name=self.__class__.__name__)
         self.max_length = max_length
-        self.token_embedding = layers.Embedding(input_dim=vocab_size, output_dim=factorized_dim)
+        self.token_embedding = layers.Embedding(input_dim=vocab_size, output_dim=factorized_dim, mask_zero=True)
         self.factorized_embedding = layers.Dense(embedding_dim)
         self.segment_embedding = layers.Embedding(input_dim=2, output_dim=embedding_dim)
         self.positional_embedding = layers.Embedding(input_dim=max_length, output_dim=embedding_dim)
 
     def call(self, inputs, *args, **kwargs):
-        # inputs = (token_ids OR random_masked_input_ids, type_token_ids, attention_mask)
+        # inputs = (token_ids OR masked_input_ids, type_token_ids) : no need to feed attention_mask manually.
         # |input[i]| = batch_size * max_length
         token_ids = inputs[0]
         type_token_ids = inputs[1]
-        attention_mask = inputs[2]
         # Factorized token embedding for token_ids
-        token_vectors = self.token_embedding(token_ids, **kwargs)
-        factorized_vectors = self.factorized_embedding(token_vectors, **kwargs)
+        token_vectors = self.token_embedding(token_ids)
+        factorized_vectors = self.factorized_embedding(token_vectors)
+        # get attention_mask from embedding.compute_mask(token_ids)
+        attention_mask = self.token_embedding.compute_mask(token_ids)
+        attention_mask = attention_mask[:, tf.newaxis, tf.newaxis, :]
         # Positional embedding
         positions = tf.range(start=0, limit=self.max_length, delta=1)
-        position_vectors = self.positional_embedding(positions, **kwargs)
+        position_vectors = self.positional_embedding(positions)
         # Segment embedding
-        segment_vector = self.segment_embedding(type_token_ids, **kwargs)
+        segment_vector = self.segment_embedding(type_token_ids)
         outputs = factorized_vectors + position_vectors + segment_vector
-        print('embedding outputs', tf.shape(outputs))
-        print('embedding attention_mask', tf.shape(attention_mask))
+        return outputs, attention_mask
+
+
+class MaskedLanguageModelEmbeddingLayer(layers.Layer):
+    def __init__(self, max_length, vocab_size, embedding_dim):
+        """
+        :param max_length: tokenizer.max_length
+        :param vocab_size: tokenizer.vocab_size for input_dim of Embedding
+        :param embedding_dim: final embedding output dim
+        note that attention_mask will be computed from embedding.compute_mask(token_ids).
+        """
+        super(MaskedLanguageModelEmbeddingLayer, self).__init__(name=self.__class__.__name__)
+        self.max_length = max_length
+        self.token_embedding = layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim, mask_zero=True)
+        self.segment_embedding = layers.Embedding(input_dim=2, output_dim=embedding_dim)
+        self.positional_embedding = layers.Embedding(input_dim=max_length, output_dim=embedding_dim)
+
+    def call(self, inputs, *args, **kwargs):
+        # inputs = (token_ids OR masked_input_ids, type_token_ids) : no need to feed attention_mask manually.
+        # |input[i]| = batch_size * max_length
+        token_ids = inputs[0]
+        type_token_ids = inputs[1]
+        # token embedding for token_ids
+        token_vectors = self.token_embedding(token_ids)
+        # get attention_mask from embedding.compute_mask(token_ids)
+        attention_mask = self.token_embedding.compute_mask(token_ids)
+        attention_mask = attention_mask[:, tf.newaxis, tf.newaxis, :]
+        # Positional embedding
+        positions = tf.range(start=0, limit=self.max_length, delta=1)
+        position_vectors = self.positional_embedding(positions)
+        # Segment embedding
+        segment_vector = self.segment_embedding(type_token_ids)
+        outputs = token_vectors + position_vectors + segment_vector
         return outputs, attention_mask
 
 
@@ -66,13 +106,13 @@ class TransformerEncoderBlock(layers.Layer):
         # inputs = (token_vectors+position_vectors+segment_vector, tf.convert_to_tensor(attention_mask))
         query, value, key = inputs[0], inputs[0], inputs[0]
         attention_mask = inputs[1]
-        attention_output = self.attention(query=query, value=value, key=key, **kwargs)
-        attention_output = self.dropout1(attention_output, **kwargs)
-        attention_output = self.layer_normalization1(query + attention_output, **kwargs)
-        dense_output = self.feedforward(attention_output, **kwargs)
-        dense_output = self.dropout2(dense_output, **kwargs)
-        sequence_output = self.layer_normalization2(attention_output + dense_output, **kwargs)
-        return sequence_output, attention_mask
+        attention_output = self.attention(query=query, value=value, key=key, attention_mask=attention_mask)
+        attention_output = self.dropout1(attention_output)
+        attention_output = self.layer_normalization1(query + attention_output)
+        dense_output = self.feedforward(attention_output)
+        dense_output = self.dropout2(dense_output)
+        sequence_outputs = self.layer_normalization2(attention_output + dense_output)
+        return sequence_outputs, attention_mask
 
 
 class TransformerStackedEncoderLayers(layers.Layer):
@@ -121,29 +161,34 @@ class TransformerPostEncoderDenseLayer(layers.Layer):
 
     def call(self, inputs, *args, **kwargs):
         # |input[0]| = batch_size * max_length * embedding_dim
-        outputs = self.classifier(inputs[0])
-        return outputs
+        _outputs = self.classifier(inputs[0])
+        return _outputs
 
 
 if __name__ == '__main__':
     cfg = MLMConfig()
 
     tokenizer = CharLevelTokenizer().load_pretrained_tokenizer('../tokenizer/your_awesome_tokenizer.json')
+
     """
     with open('../tokenizer/samples.txt', encoding='utf-8') as samples:
         texts = samples.read().splitlines()[:5]
-    
     encoded_tf = tokenizer.encode_for_transformer(texts, add_special_tokens=True, random_mask=True)
-    
-    inputs = tf.convert_to_tensor([encoded_tf['input_ids'], encoded_tf['token_type_ids'], encoded_tf['attention_mask']])
+    model_inputs = tf.convert_to_tensor([encoded_tf['input_ids'], encoded_tf['token_type_ids']])
     """
 
-    inputs = layers.Input(shape=(3, cfg.MAX_LENGTH,), dtype=tf.int64)
-    embedding_layer = FactorizedTokSegPositEmbeddingLayer(max_length=cfg.MAX_LENGTH,
-                                                          vocab_size=tokenizer.vocab_size,
-                                                          embedding_dim=cfg.EMBEDDING_DIM,
-                                                          factorized_dim=cfg.FACTORIZED_DIM)
-    embedding = embedding_layer(inputs)
+    model_inputs = layers.Input(shape=(2, cfg.MAX_LENGTH,), dtype=tf.int64, name='CharLevelInputLayer')
+    """
+    embedding_layer = FactorizedEmbeddingLayer(max_length=cfg.MAX_LENGTH,
+                                               vocab_size=tokenizer.vocab_size,
+                                               embedding_dim=cfg.EMBEDDING_DIM,
+                                               factorized_dim=cfg.FACTORIZED_DIM)
+    """
+    embedding_layer = MaskedLanguageModelEmbeddingLayer(max_length=cfg.MAX_LENGTH,
+                                                        vocab_size=tokenizer.vocab_size,
+                                                        embedding_dim=cfg.EMBEDDING_DIM)
+
+    embedding = embedding_layer(model_inputs)
     transformer_encoder_layer = TransformerStackedEncoderLayers(num_layers=cfg.NUM_LAYERS,
                                                                 cross_layer_sharing=cfg.CROSS_LAYER_SHARING,
                                                                 embedding_dim=cfg.EMBEDDING_DIM,
@@ -153,12 +198,12 @@ if __name__ == '__main__':
     sequence_output = transformer_encoder_layer(embedding)
     post_encoder_layer = TransformerPostEncoderDenseLayer(embedding_dim=cfg.EMBEDDING_DIM,
                                                           vocab_size=tokenizer.vocab_size)
-    outputs = post_encoder_layer(sequence_output)
+    model_outputs = post_encoder_layer(sequence_output)
 
-    model = keras.Model(inputs, outputs)
+    model = keras.Model(model_inputs, model_outputs, name='BOAT')
 
-    optimizer = keras.optimizers.Adam(learning_rate=cfg.LEARNING_RATE)
+    optimizer = keras.optimizers.Adam(learning_rate=cfg.LEARNING_RATE, beta_1=0.9, beta_2=0.999)
     loss = keras.losses.SparseCategoricalCrossentropy()
     model.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
 
-    model.summary(line_length=250)
+    model.summary(line_length=200)
