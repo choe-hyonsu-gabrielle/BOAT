43112730592e7858ab50ee8460c147d84df6cfb1
Choe Hyonsu Gabrielle <choe.hyonsu.gabrielle@gmail.com>
2022-02-27 02:10:33 +0900

    dynamic strip

--- gabrielle/pretrained/trainer.py
+++ gabrielle/pretrained/trainer.py
@@ -50,7 +50,7 @@ def get_model(config, name=None, plot=False):
     if plot:
         keras.utils.plot_model(model, 'your_awesome_model.png', show_shapes=True)
 
-    model.summary(line_length=200)
+    model.summary(line_length=150)
 
     for k, v in config.__dict__.items():
         if not k.startswith('__'):
